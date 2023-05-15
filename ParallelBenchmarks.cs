ceaa0c31740648cfe3f1a7d2cc3fac136758e00b
Prem Parihar <prpariha@microsoft.com>
2023-05-15 15:52:42 +0530

    initial commit

---
+++ ParallelBenchmarks.cs
@@ -0,0 +1,41 @@
+﻿namespace ParallelBenchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Engines;
+    using BenchmarkDotNet.Running;
+
+    [SimpleJob(RunStrategy.ColdStart, iterationCount: 10)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class ParallelBenchmarks
+    {
+        readonly int maxthread = 5;
+        readonly IEnumerable<int> list = Enumerable.Range(1, 200);
+
+        [Benchmark]
+        public async Task SemaphoreSlim() => await ParallelTask.SemaphoreSlim(maxthread, list);
+
+        [Benchmark]
+        public async Task ParallelForEach() => await ParallelTask.ParallelForEach(maxthread, list);
+    }
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ParallelBenchmarks>();
+
+            /* Uncomment following code to debug */
+
+            //int maxthread = 30;
+            //var totalRecord = 1000;
+            //IEnumerable<int> list = Enumerable.Range(1, totalRecord);
+            //await ParallelTask.SemaphoreSlim(maxthread, list);
+            //await ParallelTask.ParallelForEach(maxthread, list);
+            //Console.ReadLine();
+        }
+    }
+
+}
---
+++ ParallelTask.cs
@@ -0,0 +1,82 @@
+﻿namespace ParallelBenchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class ParallelTask
+    {
+        static int delay = 50;
+        public async static Task SemaphoreSlim(int parallelThread, IEnumerable<int> list)
+        {
+            var COUNTER = 0;
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var allTasks = new List<Task>();
+            var throttler = new SemaphoreSlim(initialCount: parallelThread);
+
+
+            foreach (var quoteId in list)
+            {
+
+                await throttler.WaitAsync();
+                allTasks.Add(
+                     Task.Run(async () =>
+                     {
+                         try
+                         {
+                             Thread.Sleep(delay);
+                             await Task.Run(() =>
+                             {
+                                 Interlocked.Increment(ref COUNTER);
+                             });
+                         }
+
+                         finally
+                         {
+                             throttler.Release();
+                         }
+                     }));
+            }
+            await Task.WhenAll(allTasks);
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            var elapsedTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"[{nameof(SemaphoreSlim)}] COUNTER: {COUNTER} ElapsedMilliseconds: {elapsedTime}");
+        }
+
+        public static async Task ParallelForEach(int parallelThread, IEnumerable<int> list)
+        {
+            //  var list = Enumerable.Range(1, totalRecord);
+            var COUNTER = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var parallelOptions = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = parallelThread
+            };
+
+            await Parallel.ForEachAsync(list, parallelOptions, async (i, token) =>
+            {
+                Thread.Sleep(delay);
+                await Task.Run(() =>
+                {
+                    Interlocked.Increment(ref COUNTER);
+                });
+
+            });
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            var elapsedTime = stopWatch.ElapsedMilliseconds;
+
+            Console.WriteLine($"[{nameof(ParallelForEach)}] COUNTER: {COUNTER} ElapsedMilliseconds: {elapsedTime}");
+        }
+    }
+
+}
